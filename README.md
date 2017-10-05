4e7633b937b218c31371843ca33748cb28583815
Richard Guay <raguay@customct.com>
2017-10-05 11:21:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Flexible Trash
+
+Plugin for [fman.io](https://fman.io) that will trash files and directories using Finder if it is avaliable, or simple use the command line **rm** if it isn't. This is needed because I often close out Finder since I'm using alternative file managers.
+
+You can install this plugin by pressing `<shift+cmd+p>` to open the command pallet. Then type `install plugin`. Look for the `Flexible Trash` plugin and select it.
+
+
+### Usage
+
+This plugin over rides the delete function to delete files and directories. It is transparent to normal operation.
+
+### Features
+
+- The ability to delete files/directories even if Finder isn't running.
---
+++ flexibletrash/__init__.py
@@ -0,0 +1,50 @@
+#
+# Load the libraries that are used in these commands.
+#
+from fman import DirectoryPaneCommand, show_alert,  show_prompt, show_status_message, clear_status_message, PLATFORM, YES, NO, DATA_DIRECTORY, FMAN_VERSION
+import subprocess, re
+
+#
+# Function:    NewMoveToTrash
+#
+# Description:  This command moves files and directories to the trash if Finder is
+#                       running. Otherwise, it deletes them using a low level delete.
+#
+class NewMoveToTrash(DirectoryPaneCommand):
+
+    aliases = ('Delete', 'Move to trash', 'Move to recycle bin')
+
+    def __call__(self):
+        fmanv = 0
+        if isinstance(FMAN_VERSION, str):
+            fmanvParts = FMAN_VERSION.split('.')
+            if fmanvParts[0] == '0':
+                fmanv = int(fmanvParts[1])
+        else:
+            fmanv = FMAN_VERSION
+        to_delete = self.get_chosen_files()
+        if not to_delete:
+            show_alert('No file is selected!')
+            return
+        if len(to_delete) > 1:
+            description = 'these %d items' % len(to_delete)
+        else:
+            description = to_delete[0]
+        trash = 'Recycle Bin' if PLATFORM == 'Windows' else 'Trash'
+        choice = show_alert(
+            "Do you really want to move %s to the %s?" % (description, trash), YES | NO, YES
+        )
+        if choice & YES:
+            if PLATFORM != 'Mac':
+                move_to_trash(*to_delete)
+            else:
+                pluginLoc = DATA_DIRECTORY + "/Plugins/FlexibleTrash/"
+                if fmanv > 4:
+                    pluginLoc = DATA_DIRECTORY + "/Plugins/Third-party/FlexibleTrash/"
+                finderFound = subprocess.run([pluginLoc + "finderRunning"], stdout=subprocess.PIPE)
+                if re.search('not running',finderFound.stdout.decode("utf-8")) == None:
+                    for item in to_delete:
+                        subprocess.run([pluginLoc + "trash", item])
+                else:
+                    for item in to_delete:
+                        subprocess.run(["rm", "-Rf", item])
